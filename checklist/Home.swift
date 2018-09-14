498c297b7a313fdac9b916bc89946beeb0cbf00b
Zhe Jia <zhejia@Zhes-MacBook-Pro.local>
2018-09-14 09:21:43 -0500

    Sync selected state for todo cells.

--- checklist/Home.swift
+++ checklist/Home.swift
@@ -9,7 +9,7 @@ class Home: UITableViewController {
     CheckListItem(checked: true, content: "Third Item"),
     CheckListItem(checked: false, content: "Fourth Item"),
     CheckListItem(checked: false, content: "Fivth Item"),
-    CheckListItem(checked: true, content: "sixth Item"),
+    CheckListItem(checked: true, content: "Sixth Item"),
     CheckListItem(checked: true, content: "Seventh Item"),
     CheckListItem(checked: true, content: "Eighth Item"),
     CheckListItem(checked: false, content: "Nineth Item"),
@@ -18,7 +18,7 @@ class Home: UITableViewController {
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    tableView.register(CheckListCell.self, forCellReuseIdentifier: checkListCell)
+    tableView.register(CheckListCell.self, forCellReuseIdentifier: CheckListCell.identifier)
   }
   
   override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -27,17 +27,15 @@ class Home: UITableViewController {
   
   override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     let cell = tableView.dequeueReusableCell(withIdentifier: checkListCell, for: indexPath) as! CheckListCell
-    //cell.selectionStyle = .none
     cell.updateViews(todo: todos[indexPath.row])
     return cell
   }
   
   override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-    let cell = tableView.cellForRow(at: indexPath) as! CheckListCell
     todos[indexPath.row].checked = !todos[indexPath.row].checked
-    cell.updateChecked(todo: todos[indexPath.row])
+    // tell tableView to reloadCell
+    tableView.reloadRows(at: [indexPath], with: .none)
   }
-
 }
 
 
